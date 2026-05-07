960a77dd2653394acf9d131844585e60ad629dcf
rakibul263 <rakibulhasanshuvo206@gmail.com>
2026-05-07 15:09:54 +0600

    Problem 2 Solve Done

--- solutions.ts
+++ solutions.ts
@@ -4,3 +4,14 @@ function filterEvenNumbers(numbers: number[]) {
   console.log(evenNumbers);
 }
 filterEvenNumbers([1, 2, 3, 4, 5, 6]);
+
+// Problem - 02
+function reverseString(input: string) {
+  let reverse = "";
+  for (let i = input.length - 1; i >= 0; i--) {
+    reverse += input[i];
+  }
+  console.log(reverse);
+}
+
+reverseString("typescript");
